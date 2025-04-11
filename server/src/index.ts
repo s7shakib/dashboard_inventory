6c82813fb0a15a387feb1c30974f47d02d98a0e0
Mohammad Sultani <132396736+s7shakib@users.noreply.github.com>
2025-04-10 22:54:43 -0400

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import bodyParser from "body-parser";
+import bodyParser from "body-parser"; 
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan"; 
